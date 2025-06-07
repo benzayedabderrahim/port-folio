d6b49ec25c3a7b7270a5efcbadce4805308c8f0d
benzayedabderrahim <benzayedabderrahim@gmail.com>
2025-06-07 16:17:41 +0100

    all

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
@@ -7,25 +6,7 @@ function Navbar() {
     <nav className="navbar navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
        <a className="navbar-brand" href="/">Abderrahim BENZAYED</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-          <div className="offcanvas-header">
-            <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-          </div>
-          <div className="offcanvas-body">
-            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li className="nav-item">
-                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/contacting">Contact</NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+     </div>
     </nav>
   );
 }
