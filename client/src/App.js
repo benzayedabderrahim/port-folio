683ed34e99b4ec0dbb14b29be4711eb7e5d2ab5b
Abderrahim Benzayed <benzayedabderrahim@gmail.com>
2026-04-12 22:57:07 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Contact from './contact';
 import CurrencyConverter from './convertor';
+import WeatherApp from './weather';
 
 function App() {
   return (
